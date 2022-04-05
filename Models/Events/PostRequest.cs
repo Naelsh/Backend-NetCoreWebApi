6ac69619f079361d398108a0bef6c7203874b9c1
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-05 17:18:12 +0200

    Add postrequest for events

---
+++ Models/Events/PostRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.Events
+{
+    public class PostRequest
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public int CreatorId { get; set; }
+    }
+}
