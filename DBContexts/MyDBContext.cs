4c21da0d67220355b6cdea0de99f33d7c3eb722a
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-05 13:37:22 +0200

    Add project files.

---
+++ DBContexts/MyDBContext.cs
@@ -0,0 +1,30 @@
+﻿using WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models.Users;
+
+namespace WebApi.DBContexts
+{
+    public class MyDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Use Fluent API to configure  
+
+            // Map entities to tables  
+            //modelBuilder.Entity<UserGroup>().ToTable("UserGroups");
+            modelBuilder.Entity<User>().ToTable("Users");
+
+            
+        }
+    }
+}
