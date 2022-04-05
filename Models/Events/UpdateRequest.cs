9ec57a572d059e55f50d8462106be7be1f453c32
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-05 17:25:42 +0200

    Add UpdateRequest

---
+++ Models/Events/UpdateRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models.Events
+{
+    public class UpdateRequest
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int CreatorId { get; set; }
+    }
+}
