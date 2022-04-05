428e04e1d6abcbec84bf07e0566a89ad8cccbbb7
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-05 17:14:16 +0200

    add EventItem entity class

---
+++ Entities/EventItem.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi.Entities;
+public class EventItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+
+    public User Creator { get; set; }
+    public List<User> Admins { get; set; }
+    public List<User> Participants { get; set; }
+}
