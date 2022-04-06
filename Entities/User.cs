4f17a63bded903e2c507fa14545f7179927b7a2e
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-06 12:29:37 +0200

    Update user with data anotaiont

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,12 +1,17 @@
 namespace WebApi.Entities;
 
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 public class User
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     public string LastName { get; set; }
+    [Required]
     public string Username { get; set; }
 
     [JsonIgnore]
