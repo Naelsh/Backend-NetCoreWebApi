ca8d54bf7e73454bdd91fcc5f8080a9c8f791763
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-06 16:49:11 +0200

    inject events to asp.net

--- Program.cs
+++ Program.cs
@@ -28,6 +28,7 @@ var builder = WebApplication.CreateBuilder(args);
     // configure DI for application services
     services.AddScoped<IJwtUtils, JwtUtils>();
     services.AddScoped<IUserService, UserService>();
+    services.AddScoped<IEventService, EventService>();
 }
 
 var app = builder.Build();
