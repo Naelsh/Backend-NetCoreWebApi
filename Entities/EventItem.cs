4960df8c603e7fac858252b3f6e9d0cd40407ad8
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-06 12:29:07 +0200

    update eventItem with data anotations

--- Entities/EventItem.cs
+++ Entities/EventItem.cs
@@ -1,13 +1,22 @@
-﻿namespace WebApi.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Entities;
 public class EventItem
 {
+    [Key]
     public int Id { get; set; }
+    [MaxLength(50)]
+    [Required]
     public string Title { get; set; }
+    [Required]
     public string Description { get; set; }
+    [Required]
     public DateTime StartDate { get; set; }
+    [Required]
     public DateTime EndDate { get; set; }
 
+    [Required]
     public User Creator { get; set; }
-    public List<User> Admins { get; set; }
-    public List<User> Participants { get; set; }
+    public List<User> Admins { get; set; } = new List<User>();
+    public List<User> Participants { get; set; } = new List<User>();
 }
