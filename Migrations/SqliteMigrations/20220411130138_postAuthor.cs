0c8fe1faea5b4f8f4f9cb65210ed61332a613c36
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-11 16:04:11 +0200

    migration and update to database

---
+++ Migrations/SqliteMigrations/20220411130138_postAuthor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations.SqliteMigrations
+{
+    public partial class postAuthor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "PostItems",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostItems_AuthorId",
+                table: "PostItems",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PostItems_Users_AuthorId",
+                table: "PostItems",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PostItems_Users_AuthorId",
+                table: "PostItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PostItems_AuthorId",
+                table: "PostItems");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "PostItems");
+        }
+    }
+}
