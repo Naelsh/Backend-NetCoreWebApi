eebf341b09b28174d5da4affd4fdd0fa9102fd07
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-14 09:35:13 +0200

    Add base controller for getting user info

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class BaseController : ControllerBase
+    {
+        protected User GetAuthenticatedUser()
+        {
+            return (User)HttpContext.Items["User"];
+        }
+
+        protected int GetAuthenticatedUserId()
+        {
+            return ((User)HttpContext.Items["User"]).Id;
+        }
+    }
+}
